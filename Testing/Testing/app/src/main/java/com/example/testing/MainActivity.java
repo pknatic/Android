bcd29133a80a84e2bc9aef49559b79097a0dab3e
Pknatic <info.pkrajput@gmail.com>
2023-06-06 16:39:22 +0530

    a

--- Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
+++ Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    heading = TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
