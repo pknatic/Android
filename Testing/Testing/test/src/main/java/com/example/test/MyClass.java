a2f1d2d3fcd925a1d29a68c0502d28903fd230f4
Pknatic <info.pkrajput@gmail.com>
2023-06-06 10:05:04 +0530

    a

--- Testing/Testing/test/src/main/java/com/example/test/MyClass.java
+++ Testing/Testing/test/src/main/java/com/example/test/MyClass.java
@@ -1,7 +1,16 @@
 package com.example.test;
 
 public class MyClass {
+    public static int func(int a,int b){
+        int x = a * b;
+        return x;
+    }
     public static void main(String[] args) {
         System.out.println("Hello Pknatic!!");
+        int a = 6 ;
+        int b = 7;
+        int c;
+        c = func(a,b);
+        System.out.println(c);
     }
 }
--- project_3/README.md
+++ project_3/README.md
@@ -1,1 +1,2 @@
 # Multiplication Table Of Given Number
+[![N|Solid](https://lh3.googleusercontent.com/qwq46W93q9KXYGofoK1fOB4D6LfZ7DRZWbveSvO4Z9Kadcl5ZjP7WQBGZ4gHQ6FdmNY=h900-rw)]
