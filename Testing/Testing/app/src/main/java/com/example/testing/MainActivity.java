02426891d29115d52afc4f854bca036be543223c
Pknatic <info.pkrajput@gmail.com>
2023-06-20 06:23:11 +0530

    20-06-2023

--- Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
+++ Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button = findViewById(R.id.button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
