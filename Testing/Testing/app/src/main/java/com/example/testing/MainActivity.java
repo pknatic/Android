d49cf89324b056afb0eb26bd936f4d5179d23e0c
Pknatic <info.pkrajput@gmail.com>
2023-06-23 18:07:06 +0530

    a

--- Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
+++ Testing/Testing/app/src/main/java/com/example/testing/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-Button button = findViewById(R.id.button);
+public class MainActivity extends AppCompatActivity
+{
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 }
