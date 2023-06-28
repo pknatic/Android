004d7f61ac353a6c8156e25cfb055576dc1fc285
Pknatic <info.pkrajput@gmail.com>
2023-06-28 11:58:24 +0530

    update

--- Testing/Learn_from_book/app/src/main/java/com/example/learn_from_book/MainActivity.java
+++ Testing/Learn_from_book/app/src/main/java/com/example/learn_from_book/MainActivity.java
@@ -1,18 +1,14 @@
-package com.example.testing;
+package com.example.learn_from_book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
--- Testing/Testing2/app/src/main/java/com/example/testing2/MainActivity.java
+++ Testing/Testing2/app/src/main/java/com/example/testing2/MainActivity.java
@@ -1,18 +1,14 @@
-package com.example.testing;
+package com.example.testing2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
